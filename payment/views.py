c59125cb70d7d972a83e997dbea727c1c23523c0
abdullasalimov <abdullasalimov7@gmail.com>
2022-09-14 16:33:56 +0500

    asdf

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,11 @@
+import imp
+from django.conf import settings
 from django.views.generic.base import TemplateView
 
 class IndexPageView(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['key'] = settings.STRIPE_PUBLIC_KEY
+        return context
